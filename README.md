b28048b745869e24b1afcff4293ecf3decd280f8
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-01 23:45:06 +0530

    resume is creating

---
+++ README.md
@@ -0,0 +1,1 @@
+# CV of Ankur Halder
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Hello, World!</h1>
+      <p>This is a Ankur Halder&apos;s CV.</p>
+    </Fragment>
+  );
+}
+
+export default App;
