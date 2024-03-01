5f9570054dda7502fdbde4723f1794c919e3be76
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-02 01:28:24 +0530

    Add new data modules

--- data/index.js
+++ data/index.js
@@ -1,1 +1,8 @@
 export { default as personalInfo } from "./personalInfo";
+export { default as education } from "./education";
+export { default as skills } from "./skills";
+export { default as internships } from "./internships";
+export { default as courses } from "./courses";
+export { default as projects } from "./projects";
+export { default as sotial } from "./social";
+export { default as contact } from "./contact";
