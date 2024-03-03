10c0e593b2217aa3cb5b0308f593e400d3835bdf
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-03 18:56:29 +0530

    Refactor PersonalInfo component styling

--- components/PersonalInfo.jsx
+++ components/PersonalInfo.jsx
@@ -13,41 +13,41 @@ function PersonalInfo() {
             src={personalInfo.imageUrl}
             alt={personalInfo.name}
           />
-          <div className="info">
-            <div className="details">
-              <p className="detail">
-                <strong>Age:</strong> {personalInfo.age}
-              </p>
-              <p className="detail">
-                <strong>Gender:</strong> {personalInfo.gender}
-              </p>
-              <p className="detail">
-                <strong>Date of Birth:</strong> {personalInfo.dateOfBirth}
-              </p>
-              <p className="detail">
-                <strong>Languages Known:</strong>{" "}
-                {personalInfo.languagesKnown.join(", ")}
-              </p>
-              <p className="detail">
-                <strong>Address:</strong> {personalInfo.address}
-              </p>
-              <p className="detail">
-                <strong>Email:</strong> {personalInfo.email}
-              </p>
-              <p className="detail">
-                <strong>Website:</strong>{" "}
-                <a
-                  className="website-link"
-                  href={`https://${personalInfo.website}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {personalInfo.website}
-                </a>
-              </p>
-            </div>
+
+          <div className="details">
+            <p className="detail">
+              <strong>Age:</strong> {personalInfo.age}
+            </p>
+            <p className="detail">
+              <strong>Gender:</strong> {personalInfo.gender}
+            </p>
+            <p className="detail">
+              <strong>Date of Birth:</strong> {personalInfo.dateOfBirth}
+            </p>
+            <p className="detail">
+              <strong>Languages Known:</strong>{" "}
+              {personalInfo.languagesKnown.join(", ")}
+            </p>
+            <p className="detail">
+              <strong>Address:</strong> {personalInfo.address}
+            </p>
+            <p className="detail">
+              <strong>Email:</strong> {personalInfo.email}
+            </p>
+            <p className="detail">
+              <strong>Website:</strong>{" "}
+              <a
+                className="website-link"
+                href={`https://${personalInfo.website}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {personalInfo.website}
+              </a>
+            </p>
           </div>
         </div>
+
         <p className="about">
           <strong>About:</strong> {personalInfo.about}
         </p>
