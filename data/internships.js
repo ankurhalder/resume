51edaa395e9f45d8bdffed322bfa4cf7f6431cfb
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-03 22:57:44 +0530

    Fix certificateImage typo and update internship descriptions

--- data/internships.js
+++ data/internships.js
@@ -8,10 +8,34 @@ const internships = [
     topic: "Web Development",
     certificationCredential:
       "https://drive.google.com/file/d/1UTuhpUyL-AEO9VyOQgXhRHm-IyR-CVvb/view",
-
-    cetificateImage: "codsoft.jpg",
+    certificateImage: "codsoft.jpg",
     description:
-      "During my internship at CODSOFT, I had the opportunity to work remotely and gain hands-on experience in web development. I collaborated with the development team to create and enhance web applications, focusing on front-end and back-end development tasks. My responsibilities included implementing new features, troubleshooting issues, and optimizing website performance. Throughout the internship, I deepened my understanding of web technologies such as HTML, CSS, JavaScript, react.js, node.js and mongoDB  and gained practical experience with frameworks and libraries. Additionally, I learned to effectively communicate and collaborate in a virtual work environment, demonstrating adaptability and teamwork.",
+      "During my internship at CODSOFT, I had the opportunity to work remotely and gain hands-on experience in web development. I collaborated with the development team to create and enhance web applications, focusing on front-end and back-end development tasks. My responsibilities included implementing new features, troubleshooting issues, and optimizing website performance. Throughout the internship, I deepened my understanding of web technologies such as HTML, CSS, JavaScript, react.js, node.js, and mongoDB and gained practical experience with frameworks and libraries. Additionally, I learned to effectively communicate and collaborate in a virtual work environment, demonstrating adaptability and teamwork.",
+  },
+  {
+    title: "Web Development Intern",
+    company: "OCTANET SERVICES PVT LTD.",
+    location: "Unknown",
+    startDate: "February 1, 2024",
+    endDate: "March 1, 2024",
+    topic: "Web Development",
+    certificationCredential:
+      "https://verification.givemycertificate.com/v/Oc66c11f-4/ae-452e-80d5-53141dcla3de",
+    certificateImage: "",
+    description:
+      "During the internship period, Ankur Halder demonstrated exceptional dedication, enthusiasm, and a strong willingness to learn. They actively engaged in various projects and tasks assigned to them, exhibiting remarkable skills and a high level of professionalism.",
+  },
+  {
+    title: "Intern",
+    company: "InternPe",
+    location: "Remote",
+    startDate: "January 29, 2024",
+    endDate: "February 25, 2024",
+    topic: "Python Programming",
+    certificationCredential: "",
+    certificateImage: "",
+    description:
+      "During their working period, Ankur Halder was found to be a sincere and dedicated intern with a professional attitude and very good knowledge of the job.",
   },
 ];
 
