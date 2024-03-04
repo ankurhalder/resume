37bde49e5b02d981fcad5448ce69f8b5e555cf38
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-03-04 21:14:39 +0530

    Refactor social media links and styles

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -2,19 +2,21 @@ import socialMedia from "../data/social";
 function SocialMediaLinks() {
   return (
     <div className="social-media-links">
+      <h2 className="section-title">Connect with me</h2>
       {Object.keys(socialMedia).map((platform, index) => (
-        <a
-          key={index}
-          href={socialMedia[platform].url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={socialMedia[platform].image}
-            alt={platform}
-            className="social-media-icon"
-          />
-        </a>
+        <div key={index} className="right-side">
+          <a
+            href={socialMedia[platform].url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={socialMedia[platform].image}
+              alt={platform}
+              className="social-media-icon"
+            />
+          </a>
+        </div>
       ))}
     </div>
   );
